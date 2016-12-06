1b5367eab9a7a5e42dcabb64a9870a5891e0d074
Umabot <umabot@users.noreply.github.com>
2016-12-06 13:15:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,42 @@
-'use strict';
-
-const _ = require('lodash');
-const Script = require('smooch-bot').Script;
-
-const scriptRules = require('./script.json');
-
-module.exports = new Script({
-    processing: {
-        //prompt: (bot) => bot.say('Beep boop...'),
-        receive: () => 'processing'
-    },
-
-    start: {
-        receive: (bot) => {
-            return bot.say('So you want to learn about Silvio? Just say HELLO to get started.')
-                .then(() => 'speak');
-        }
-    },
-
-    speak: {
-        receive: (bot, message) => {
-
-            let upperText = message.text.trim().toUpperCase();
-
-            function updateSilent() {
-                switch (upperText) {
-                    case "CONNECT ME":
-                        return bot.setProp("silent", true);
-                    case "DISCONNECT":
-                        return bot.setProp("silent", false);
-                    default:
-                        return Promise.resolve();
-                }
-            }
-
-            function getSilent() {
-                return bot.getProp("silent");
-            }
-
-            function processMessage(isSilent) {
-                if (isSilent) {
-                    return Promise.resolve("speak");
-                }
-
-                if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
-                }
-
-                var response = scriptRules[upperText];
-                var lines = response.split('\n');
-
-                var p = Promise.resolve();
-                _.each(lines, function(line) {
-                    line = line.trim();
-                    p = p.then(function() {
-                        console.log(line);
-                        return bot.say(line);
-                    });
-                })
-
-                return p.then(() => 'speak');
-            }
-
-            return updateSilent()
-                .then(getSilent)
-                .then(processMessage);
-        }
-    }
-});
+'use strict'; 
+const Script = require('smooch-bot').Script; 
+module.exports = new Script({ 
+processing: { 
+prompt: (bot) => bot.say('Beep boop...'), 
+receive: () => 'processing'
+}, 
+start: { 
+receive: (bot) => { 
+return bot.say('Hi! I\'m Silvio\'s Bot! I can answer questions about Silvio\'s career') 
+.then(() => 'askName'); 
+} 
+}, 
+askName: { 
+prompt: (bot) => bot.say('What\'s your name?'), 
+receive: (bot, message) => { 
+const name = message.text; 
+return bot.setProp('name', name) 
+.then(() => bot.say(`Great! I'll call you ${name}
+Is that OK? %[Yes](postback:yes) %[No](postback:no)`)) 
+.then(() => 'askExperience1990'); 
+} 
+}, 
+askExperience1990: { 
+prompt: (bot) => bot.say('Do you want to know how Silvio started in IT?'), 
+receive: (bot, message) => { 
+const yesno = message.text; 
+return bot.setProp('name', name) 
+.then(() => bot.say(`Great! I'll call you ${name}
+Is that OK? %[Yes](postback:yes) %[No](postback:no)`)) 
+.then(() => 'askExperience'); 
+} 
+}, 
+finish: { 
+receive: (bot, message) => { 
+return bot.getProp('name') 
+.then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+'teach me how to do anything else!')) 
+.then(() => 'finish'); 
+} 
+} 
+}); 
